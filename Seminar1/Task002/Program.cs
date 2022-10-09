83675f72c0c21155a9af1a56dba55d7333638093
Aleksandr <platunov.aleksandr@mail.ru>
2022-10-09 21:06:45 +0300

    Домашнее задание по первому семинару

--- Seminar1/Task002/Program.cs
+++ Seminar1/Task002/Program.cs
@@ -1,15 +1,17 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите цело число a: ");
+Console.Write("Введите цело число A: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите целое число b: ");
+Console.Write("Введите целое число B: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
 
 if (a > b){
-    Console.Write("Наибольшее число: " + a);
+    Console.Write("Наибольшее число A = " + a);
 }
-else{
-    Console.Write("Наибольшее число: " + b);
+else if (b > a){
+    Console.Write("Наибольшее число B = " + b);
 }
- 
+else{ 
+    Console.Write("Числа A и B равны = " + a);
+}
