6e9b8ea20e140bc8f4ba11f005c4dc9d8a6c0c78
Aleksandr <platunov.aleksandr@mail.ru>
2022-10-13 02:22:32 +0300

    Пытаемся устранить баги

--- Seminar2/Task010/Program.cs
+++ Seminar2/Task010/Program.cs
@@ -1,16 +1,22 @@
 ﻿// Задача 10: Написать программу, которая принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
 
-Console.Write("Введите трёхзначное число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-while (n < 99 | n > 1000)
+while (true)
 {
-    Console.Write("Вы ввели не трехзначное число, попробуйте еще раз: ");
-    n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+         while (number < 99 | number > 1000)
+         {
+            Console.Write("Вы ввели не трехзначное число, попробуйте еще раз: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.Write("Вторая цифра числа: " + Convert.ToInt32((number / 10) % 10));
+        break;
+    }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
 }
-
-    Console.Write("Вторая цифра числа: " + Convert.ToInt32((n / 10) % 10));
 
 /*
     // Второй вариант, но у меня так и не получилось довести его до ума((((
---
+++ Seminar2/Task013/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+        Console.WriteLine("{0}", number);
+        while (number < 99 | number > 1000)
+        {
+            Console.Write("Вы ввели не трехзначное число, попробуйте еще раз: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.Write("Вторая цифра числа: " + Convert.ToInt32((number / 10) % 10));
+        break;
+    }
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
