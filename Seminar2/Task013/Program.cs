bbddf264880a590c80a70a708fd2c1c0a5ee876d
Aleksandr <platunov.aleksandr@mail.ru>
2022-10-14 00:53:07 +0300

    Task013

--- Seminar2/Task013/Program.cs
+++ Seminar2/Task013/Program.cs
@@ -1,21 +1,16 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// Задача 13: Напишите программу, которая выводит третью (слева) цифру заданного числа или сообщает, что третьей цифры нет.
 
-
-
-while (true)
+Console.WriteLine("Введите любое число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ while (n > 1000)
 {
-    Console.Write("Введите число: ");
-    string text = Console.ReadLine();
-    if (int.TryParse(text, out int number))
-    {
-        Console.WriteLine("{0}", number);
-        while (number < 99 | number > 1000)
-        {
-            Console.Write("Вы ввели не трехзначное число, попробуйте еще раз: ");
-            number = Convert.ToInt32(Console.ReadLine());
-        }
-        Console.Write("Вторая цифра числа: " + Convert.ToInt32((number / 10) % 10));
-        break;
-    }
-    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    n = n/10;
 }
+
+while (n < 100)
+{
+    Console.WriteLine("В введенном числе нет третьей цифры, давай еще разок: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(n % 10);
+
