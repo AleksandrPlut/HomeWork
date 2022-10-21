3a2c14f98a9f9dfc43da16480eae78bcb2a9d7e0
Aleksandr <platunov.aleksandr@mail.ru>
2022-10-22 01:01:21 +0300

    Смидлил вариант решение 27 задачи

--- Seminar4/Task027/Program.cs
+++ Seminar4/Task027/Program.cs
@@ -10,3 +10,17 @@ while (n > 0)
  }
 Console.WriteLine(result);
 
+
+// Слизал с семинара, для того чтоб было, не для оценки.
+// Даже смог разобраться))
+/*
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+int result = 0;
+for (int i = 0; i < length; i++)
+{
+    result = result + Convert.ToInt32(Convert.ToString(number[i]));
+}
+Console.WriteLine(result);
+*/
--- Seminar4/Task029/Program.cs
+++ Seminar4/Task029/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 29. Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
 
-Console.WriteLine("Hello, World!");
+int  [] arr = new int [0];
+void newArray(int [] arr)
+{
+    Console.Write("Введите длинну массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int [] array = new int [n];
+}
+void newArrayP (int [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write("Введите элементы массива: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+}
+//PrintArray(arr);
